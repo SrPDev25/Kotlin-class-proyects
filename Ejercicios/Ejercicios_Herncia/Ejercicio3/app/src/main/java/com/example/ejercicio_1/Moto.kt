ae02bfaf0b8565bbd60d1b63854f59111d3e201f
SrPollo25 <ortegachkn25@gmail.com>
2022-11-02 10:16:44 +0100

    Avanzando

--- Ejercicios/Ejercicios_Herncia/Ejercicio3/app/src/main/java/com/example/ejercicio_1/Moto.kt
+++ Ejercicios/Ejercicios_Herncia/Ejercicio3/app/src/main/java/com/example/ejercicio_1/Moto.kt
@@ -2,7 +2,7 @@ package com.example.ejercicio_1
 
 class Moto(modelo:String, color:String, marca:String, maletero:Boolean, nPlazas:Int): Vehiculo(modelo, color, marca) {
     var maletero:Boolean=false
-        set(newField:Boolean){
+        set(newField){
             field=newField
         }
 
--- Ejercicios/UT4MD_Ortega_Daniel/E1MDOrtegaDaniel/app/src/main/java/com/example/e1mdortegadaniel/MainActivity.kt
+++ Ejercicios/UT4MD_Ortega_Daniel/E1MDOrtegaDaniel/app/src/main/java/com/example/e1mdortegadaniel/MainActivity.kt
@@ -5,11 +5,15 @@ import android.os.Bundle
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
-
+import com.example.e1mdortegadaniel.databinding.ActivityMainBinding
+//añadir: https://www.google.com/url?sa=i&url=https%3A%2F%2Flibrosking.com%2Ftienda%2F&psig=AOvVaw26YJd-F5opP9kYG4jt_SKS&ust=1667465628639000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCIC1qtGPj_sCFQAAAAAdAAAAABAV
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        actionBar?.hide()
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
