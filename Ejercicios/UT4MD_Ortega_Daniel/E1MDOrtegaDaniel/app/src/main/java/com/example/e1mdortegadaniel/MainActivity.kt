11de9972022be630531df03a7c3a2352400aca48
SrPollo25 <ortegachkn25@gmail.com>
2022-11-02 12:47:42 +0100

    Trabajando en web image

--- Ejercicios/UT4MD_Ortega_Daniel/E1MDOrtegaDaniel/app/src/main/java/com/example/e1mdortegadaniel/MainActivity.kt
+++ Ejercicios/UT4MD_Ortega_Daniel/E1MDOrtegaDaniel/app/src/main/java/com/example/e1mdortegadaniel/MainActivity.kt
@@ -14,6 +14,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        binding.imgLogo.
     }
 }
