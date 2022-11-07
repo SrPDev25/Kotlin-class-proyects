e93c8800c6d9c7317e46c8cfbf92e8cba4392bb1
SrPollo25 <ortegachkn25@gmail.com>
2022-11-07 21:10:09 +0100

    floating in progress

--- Ejercicios/UT4MD_Ortega_Daniel/E1MDOrtegaDaniel/app/src/main/java/com/example/e1mdortegadaniel/MainActivity.kt
+++ Ejercicios/UT4MD_Ortega_Daniel/E1MDOrtegaDaniel/app/src/main/java/com/example/e1mdortegadaniel/MainActivity.kt
@@ -10,14 +10,28 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         Glide.with(this)
             .load("https://librosking.com/wp-content/uploads/2022/04/Logo-Libros-king-2022.1.png")
-            .into(binding.content.imgLogo)
-        binding.content.whiteLayout.background.alpha=200
+            .into(binding.imgLogo)
+        binding.whiteLayout.background.alpha=200
+
+        binding.main.buttonLogin.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus){
+                /*val placeHolder :LinearLayout = findViewById<LinearLayout>(binding.loginLayout)
+                layoutInflater.inflate(R.layout.second_layout, placeHolder)*/
+            }
+        }
+
+
+
+
+
+
+
     }
 }
 
