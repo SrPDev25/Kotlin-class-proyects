1deee0f711957a8ab49f488cc8a4f4526f85ef11
Daniel Ortega <daniel.ortega.alu@santamaria-artesano.org>
2023-01-10 11:39:22 +0100

    Empezando el ejercicio 7

--- Ejercicios/Ut7Ej7OrtegaDaniel/app/src/main/java/com/example/ut7ej7ortegadaniel/FirstActivity.kt
+++ Ejercicios/Ut7Ej7OrtegaDaniel/app/src/main/java/com/example/ut7ej7ortegadaniel/FirstActivity.kt
@@ -1,14 +1,12 @@
 package com.example.ut7ej7ortegadaniel
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
     }
-
-
 }
