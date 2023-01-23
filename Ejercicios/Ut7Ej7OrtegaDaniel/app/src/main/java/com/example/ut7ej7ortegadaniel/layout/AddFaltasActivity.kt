5fec30dc82252bdccbb5f6fb7fbee3ad49a69a19
Daniel Ortega <ortegachkn25@gmail.com>
2023-01-24 00:38:01 +0100

    terminado menos observaciones

--- Ejercicios/Ut7Ej7OrtegaDaniel/app/src/main/java/com/example/ut7ej7ortegadaniel/layout/AddFaltasActivity.kt
+++ Ejercicios/Ut7Ej7OrtegaDaniel/app/src/main/java/com/example/ut7ej7ortegadaniel/layout/AddFaltasActivity.kt
@@ -1,15 +1,107 @@
 package com.example.ut7ej7ortegadaniel.layout
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
 import com.example.ut7ej7ortegadaniel.databinding.ActivityAddFaltasBinding
+import com.mjpg.basedatos.dao.OperacionesDao
 
 class AddFaltasActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddFaltasBinding
+    lateinit var bd:OperacionesDao
+    var profesor=-1
+    var hora=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddFaltasBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        profesor=intent.getIntExtra("Profesor",-1)
         //todo todo
+        bd=OperacionesDao(this)
+        binding.dataPicker.maxDate=System.currentTimeMillis()
+        binding.buttonAnadir.setOnClickListener(){
+            comprobar()
+        }
+        binding.btnSeleccionarAlumno.setOnClickListener(){
+            val myIntent = Intent(this, AlumnosActivity::class.java)
+                .putExtra("Profesor",profesor)
+            startForResult.launch(myIntent)
+        }
+        binding.button1.setOnClickListener(){
+            resetButtonColors()
+            binding.button1.setBackgroundColor(Color.parseColor("#FFBB86FC"))
+            hora=1
+        }
+        binding.button2.setOnClickListener(){
+            resetButtonColors()
+            binding.button2.setBackgroundColor(Color.parseColor("#FFBB86FC"))
+            hora=2
+        }
+        binding.button3.setOnClickListener(){
+            resetButtonColors()
+            binding.button3.setBackgroundColor(Color.parseColor("#FFBB86FC"))
+            hora=3
+        }
+        binding.button4.setOnClickListener(){
+            resetButtonColors()
+            binding.button4.setBackgroundColor(Color.parseColor("#FFBB86FC"))
+            hora=4
+        }
+        binding.button5.setOnClickListener(){
+            resetButtonColors()
+            binding.button5.setBackgroundColor(Color.parseColor("#FFBB86FC"))
+            hora=5
+        }
+        binding.button6.setOnClickListener(){
+            resetButtonColors()
+            binding.button6.setBackgroundColor(Color.parseColor("#FFBB86FC"))
+            hora=6
+        }
+
+
+    }
+
+    private fun comprobar() {
+        var justificada = -1
+        if (binding.checkbox.isChecked)
+            justificada = 1
+        else
+            justificada = 0
+        with(binding) {
+            if (codigoAlumno.text != "0" && hora != 0) {
+                bd.addFalta(
+                    codigoAlumno.text.toString().toInt(),
+                    profesor,
+                    dataPicker.dayOfMonth.toString() + "/" + dataPicker.month.toString() + "/" + dataPicker.year.toString(),
+                    hora.toString(),
+                    justificada
+                )
+            }
+        }
+        finish()
+    }
+
+    private fun resetButtonColors() {
+        hora=0
+        binding.button1.setBackgroundColor(Color.parseColor("#FFFFFF"))
+        binding.button2.setBackgroundColor(Color.parseColor("#FFFFFF"))
+        binding.button3.setBackgroundColor(Color.parseColor("#FFFFFF"))
+        binding.button4.setBackgroundColor(Color.parseColor("#FFFFFF"))
+        binding.button5.setBackgroundColor(Color.parseColor("#FFFFFF"))
+        binding.button6.setBackgroundColor(Color.parseColor("#FFFFFF"))
+    }
+
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val intent: Int? = result.data?.getIntExtra("alumno",0)
+            if (intent!=null){
+                binding.codigoAlumno.text= intent.toInt().toString()
+            }
+        }
     }
 }
--- Ejercicios/Ut7Ej7OrtegaDaniel/app/src/main/java/com/example/ut7ej7ortegadaniel/layout/AlumnosActivity.kt
+++ Ejercicios/Ut7Ej7OrtegaDaniel/app/src/main/java/com/example/ut7ej7ortegadaniel/layout/AlumnosActivity.kt
@@ -1,41 +1,36 @@
 package com.example.ut7ej7ortegadaniel.layout
 
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.e6mdortegadaniel.Events
 import com.example.ut7ej7ortegadaniel.ListenerAdapterAlumnos
-import com.example.ut7ej7ortegadaniel.databinding.ActivityFaltasBinding
-import com.example.ut7ej7ortegadaniel.databinding.ActivityMainBinding
+import com.example.ut7ej7ortegadaniel.databinding.ActivityAlumnosBinding
 import com.mjpg.basedatos.dao.OperacionesDao
 
-class MainActivity : AppCompatActivity(), Events {
-    private lateinit var binding: ActivityMainBinding
+class AlumnosActivity: AppCompatActivity(), Events {
+    private lateinit var binding: ActivityAlumnosBinding
     private lateinit var linearLayout: LinearLayoutManager
     private var profesor=-1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        profesor=intent.getIntExtra("Profesor",-1)
         chargeLayout()
-
-        binding.buttonAdd.setOnClickListener(){
-            var myIntent=Intent(this, AddFaltasActivity::class.java)
-                .putExtra("Profesor",profesor)
-            startActivity(myIntent)
-        }
 
     }
 
     //Para que esté más ordenadito
     private fun chargeLayout() {
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityAlumnosBinding.inflate(layoutInflater)
         setContentView(binding.root)
         var db = OperacionesDao(this)
         profesor = intent.getIntExtra("Profesor", -1)
         chargeRecycler(db)
     }
 
-    private fun chargeRecycler(db:OperacionesDao) {
+    private fun chargeRecycler(db: OperacionesDao) {
         binding.recyclerview.adapter = ListenerAdapterAlumnos(db.getAlumnos(profesor), this)
         linearLayout = LinearLayoutManager(this)
         binding.recyclerview.layoutManager = linearLayout
@@ -45,9 +40,9 @@ class MainActivity : AppCompatActivity(), Events {
 
 
     override fun shortClick(pos: Int) {
-        val myIntent=Intent(this,FaltasActivity::class.java)
+        var myIntent=Intent()
             .putExtra("alumno",pos)
-        startActivity(myIntent)
+        setResult(Activity.RESULT_OK, myIntent)
+        finish()
     }
-
 }
