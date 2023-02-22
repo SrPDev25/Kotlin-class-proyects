35f23250963722477f993a1502cda4ca964d2e4a
Daniel Ortega <ortegachkn25@gmail.com>
2023-02-22 07:22:15 +0100

    Sigo en clase

--- Ejercicios/Ej9OrtegaDanielBD/app/src/main/java/com/example/ej9ortegadanielbd/view/AddUsuarioNombreFragment.kt
+++ Ejercicios/Ej9OrtegaDanielBD/app/src/main/java/com/example/ej9ortegadanielbd/view/AddUsuarioNombreFragment.kt
@@ -5,17 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.ej9ortegadanielbd.databinding.FragmentAddNombreBinding
+import com.example.ej9ortegadanielbd.dataBase.OperacionesDao
+import com.example.ej9ortegadanielbd.databinding.FragmentAddUsuarioNombreBinding
 
 class AddUsuarioNombreFragment : Fragment() {
 
-    private lateinit var binding:FragmentAddNombreBinding
+    private lateinit var binding:FragmentAddUsuarioNombreBinding
+    private var mActivity: MainActivity? = null
+    private lateinit var bd: OperacionesDao
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding=FragmentAddUsuarioNombreBinding.inflate(inflater,container,false)
+        return binding.root
     }
+
+
+
+
 }
